b3e7ddfd7052a34c0f3817b5383b9dfe84c89799
khanghungvo1211 <hungkhang02@gmail.com>
2023-10-19 21:06:18 +0700

    19_10

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Contact from './pages/Contact';
 import AllProduct from './pages/AllProduct';
 import BeautyBlog from './pages/BeautyBlog';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Forgotpassword from './pages/Forgotpassword';
+import Resetpassword from './pages/Resetpassword';
 
 
 
@@ -23,6 +26,9 @@ function App() {
               <Route path='allproduct' element={<AllProduct />} />
               <Route path='beautyblog' element={<BeautyBlog />} />
               <Route path='login' element={<Login />} />
+              <Route path='signup' element={<Signup />} />
+              <Route path='forgot-password' element={<Forgotpassword />} />
+              <Route path='reset-password' element={<Resetpassword />} />
           </Route>
         </Routes>
        </BrowserRouter>
---
+++ src/pages/Resetpassword.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { Link } from "react-router-dom";
+const Resetpassword = () => {
+  return (
+    <>
+        <Meta title={"Reset Password"} />
+      <BreadCrumb title="resetpassword" />
+    
+    </>
+  )
+}
+
+export default Resetpassword
